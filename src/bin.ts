f80b5f41791a7bd176728560cf738fbe91c2ed62
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-07-07 01:27:48 +0200

    Use folder CLI entry from bin

--- src/bin.ts
+++ src/bin.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env bun
 
-import { runCli } from "./cli.js";
+import { runCli } from "./cli/index.js";
 
 const result = await runCli(process.argv.slice(2));
-
 process.exit(result.exitCode);
